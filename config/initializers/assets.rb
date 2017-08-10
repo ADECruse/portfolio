f655fc3a43fac9e32d0f8cb7aa65a160fa82dfe8
Alan Cruse <alancruse86@gmail.com>
2017-08-10 21:00:11 +0200

    Update CSS and ERB

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( project_pages.js )
+
+Rails.application.config.assets.precompile += %w( video-js.swf vjs.eot vjs.svg vjs.ttf vjs.woff )
+Rails.application.config.assets.paths << "#{Rails.root}/app/assets/videos"
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get  '/contact', to: 'static_pages#contact'
+  get  '/about', to: 'static_pages#about'
+  get  '/work', to: 'static_pages#work'
   get  '/etch-a-sketch',  to: 'project_pages#sketch'
   get  '/caesar-cipher',  to: 'project_pages#caesar'
 
